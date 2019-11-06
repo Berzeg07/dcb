7d7c4986b7b7f0f7961d62039d0680a624bd67d1
Aslan <berezg86@gmail.com>
2019-11-06 22:02:36 +0300

    Страница Платные услуги

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -3,7 +3,7 @@ module.exports = function() {
         $.bs.init({
             server: {
                 baseDir: "./build",
-                index: "questions.html"
+                index: "services.html"
             }
         });
     });
